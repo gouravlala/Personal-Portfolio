2ed7a3403ba5185e0d054a1e4c2b211aee16048e
gouravlala <gouravlala001@gmai.com>
2023-01-04 18:39:48 +0530

    resume

--- my-portfolio/src/components/Resume/ResumeNew.js
+++ my-portfolio/src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import GouravPDF from "../../Assets/../Assets/gourav_resume.pdf"
+import GouravPDF from "../../Assets/../Assets/gouravlala_resume.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page } from "react-pdf";
 import {pdfjs} from "react-pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink=  "https://raw.githubusercontent.com/gouravlala/Personal-Portfolio/master/my-portfolio/src/Assets/gourav_resume.pdf"
+const resumeLink=  "https://raw.githubusercontent.com/gouravlala/Personal-Portfolio/master/my-portfolio/src/Assets/gouravlala_resume.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
