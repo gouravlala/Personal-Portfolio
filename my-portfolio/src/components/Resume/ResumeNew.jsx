9901e99f88cace227a258b8fa13b4a809b486a13
gouravlala <gouravlala001@gmai.com>
2023-01-04 13:09:59 +0530

    changing

--- my-portfolio/src/components/Resume/ResumeNew.jsx
+++ my-portfolio/src/components/Resume/ResumeNew.jsx
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import GouravPDF from "../../Assets/../Assets/gourav_resume.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page } from "react-pdf";
 import {pdfjs} from "react-pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-//const resumeLink = "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-// const resumeLink = "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf"
-  const resumeLink = "https://raw.githubusercontent.com/gouravlala/Personal-Portfolio/blob/master/my-portfolio/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf"
+// const   resumeLink ="https://raw.githubusercontent.com/gouravlala/Personal-Portfolio/blob/master/my-portfolio/src/Assets/gourav_%7Bresume%7D.pdf"
 
 function ResumeNew() {
+  pdfjs.GlobalWorkerOptions.workerSrc = 
+  `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
   const [width, setWidth] = useState(1200);
   
 
@@ -28,7 +27,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={GouravPDF}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -38,15 +37,20 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document file={GouravPDF} className="d-flex justify-content-center">
+          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} wrap={true}/>
+         
           </Document>
+
+
+
+
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={GouravPDF}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
